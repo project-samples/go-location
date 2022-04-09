9890befa25f6760b607debf2b4802936f8f6d0ef
Duc Nguyen <minhduc140583@gmail.com>
2022-04-09 17:26:48 +0700

    Rename conf

--- internal/app/route.go
+++ internal/app/route.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func Route(r *mux.Router, ctx context.Context, root Config) error {
-	app, err := NewApp(ctx, root)
+func Route(r *mux.Router, ctx context.Context, conf Config) error {
+	app, err := NewApp(ctx, conf)
 	if err != nil {
 		return err
 	}
